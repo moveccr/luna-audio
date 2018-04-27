f71552737e548851cdc3a2e75e851145a47d823b
moveccr <moveccr@>
2018-04-28 03:15:48 +0900

    lunaplay コンバータを書き始める。

--- lunaplay/psgpam.c
+++ lunaplay/psgpam.c
@@ -1,19 +1,45 @@
-/* vi: set ts=5: */
+/* vi: set ts=4: */
 /* TODO: LICENSE */
 
-lunapam_dev_init()
+void
+conv_u8_pam(BUFFER *dst, BUFFER *src)
 {
+	int count = src->count;
+
+	uint8_t *s = src->buf;
+	uint16_t *d = dst->buf;
+
+	for (int i = 0; i < count; i++) {
+		*d++ = PCM2_TABLE[*s++];
+	}
 }
 
-dev_write()
+void
+conv_s16BE_pam(BUFFER *dst, BUFFER *src)
 {
+	int count = src->count;
+
+	uint8_t *s = src->buf;
+	uint16_t *d = dst->buf;
+
+	for (int i = 0; i < count; i++) {
+		*d++ = PCM2_TABLE[*s ^ 0x80];
+		s += 2;
+	}
 }
 
-
-lunapam_open()
-
-lunapam_write()
+void
+conv_s16LE_pam(BUFFER *dst, BUFFER *src)
 {
-}
+	int count = src->count;
 
+	uint8_t *s = src->buf;
+	s += 1;
+	uint16_t *d = dst->buf;
+
+	for (int i = 0; i < count; i++) {
+		*d++ = PCM2_TABLE[*s ^ 0x80];
+		s += 2;
+	}
+}
 
